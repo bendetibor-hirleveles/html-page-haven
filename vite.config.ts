35f18dc3db6d204cb2b914c2b8b7bfd2fd5093d8
bendetibor-hirleveles <bende.tibor@hirleveles.hu>
2025-07-21 20:19:20 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // <-- EZ AZ ÚJ SOR: minden asset a gyökérből töltődik be
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
