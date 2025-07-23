b89200e923f6eed2c76b0d81f24927b792a737dd
bendetibor-hirleveles <bende.tibor@hirleveles.hu>
2025-07-23 10:11:06 +0200

    Update Admin.tsx

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { supabase } from "@/integrations/supabase/client";
+//import { supabase } from "@/integrations/supabase/client";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
