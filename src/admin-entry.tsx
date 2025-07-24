049d478b65b92c10d34bdbbaa0c04dc9b62edb80
bendetibor-hirleveles <bende.tibor@hirleveles.hu>
2025-07-24 10:56:44 +0200

    Update admin-entry.tsx

--- src/admin-entry.tsx
+++ src/admin-entry.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "@/index.css";
-import Admin from "@/components/admin/FooterSettingsAdmin";
+import Admin from "@/components/admin/FooterSettingsAdmin.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
