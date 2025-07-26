c90121cfa3962b09fed36c164feb8ab16a3823ba
bendetibor-hirleveles <bende.tibor@hirleveles.hu>
2025-07-26 09:21:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path, { resolve } from "path";
+import { resolve } from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
