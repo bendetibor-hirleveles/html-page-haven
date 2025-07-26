81cdf6c3b34feeb2e4307064b832fae08ef4fea5
bendetibor-hirleveles <bende.tibor@hirleveles.hu>
2025-07-26 02:07:49 +0200

    Add files via upload

---
+++ src/components/admin-entry.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { FooterSettings } from "@/components/FooterSettings";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <FooterSettings />
+  </React.StrictMode>
+);
