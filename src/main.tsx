d880473f36faf98ccecdac2a7c75000d9c1bad97
bendetibor-hirleveles <bende.tibor@hirleveles.hu>
2025-08-27 15:50:27 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+// src/main.tsx – a saját CSS-ek előtt
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import * as bootstrap from 'bootstrap';
+(window as any).bootstrap = bootstrap;
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
